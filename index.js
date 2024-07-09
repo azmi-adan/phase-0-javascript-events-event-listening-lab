8afe330ec64798bbe86fb1eca19e200acbd9e117
azmi adan <azmi.adan2032@gmail.com>
2024-07-09 08:01:26 +0300

    add all

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
+
+
 function addingEventListener() {
-}
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+  console.log("button clicked"); // move it inside the event handler
+  // other event handler code here
+})};
